7e465c8d1127147b9da23cb0e3b050563cc8c5d8
alex.zhang <gzane24@hotmail.com>
2023-03-13 00:37:15 +0800

    init

--- src/components/WebContainer/WebContainer.tsx
+++ src/components/WebContainer/WebContainer.tsx
@@ -1,19 +1,27 @@
+import { useState } from "react";
 
 interface WebContainerProps {
   url?: string | null
+  fallback?: React.ElementType
+  error?: React.ElementType
 }
 
 /**
  * WebContainer is an micro-frontend solution, which is support for mutiple mime type
  *
- * //xxxxx.html -> iframe render
+ * Resource render mapping
+ * //xxxxx.html -> html render
  * //xxxxx.md | makrdown -> markdown render
  * //xxxxx.json  -> dynamic render schema we can decide the final render continer when the json schema loaded so it's dynamic.
  *
- * steps:
+ * Steps:
  * 1. loaded the content from the url
  * 2. get the resource type from url suffix or response type(response type has hight priority than url suffix)
- * 3. get he content from the url (json, mardwon, text context) then set the content to target render, eg, iframe' srcdoc
+ * 3. get the content from the response (like json, mardwon, text context) then set the content to target render
+ *
+ * Loading Busy Status / Loading Error
+ * 1. loading Error State
+ * 2. loading busy State
  *
  * @param porps
  * @returns
