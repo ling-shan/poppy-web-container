d2d748855976ea64e32e3bd75cb28b49c4235cfe
alex.zhang <gzane24@hotmail.com>
2023-03-13 00:13:04 +0800

    init

--- src/components/WebContainer/WebContainer.tsx
+++ src/components/WebContainer/WebContainer.tsx
@@ -1,4 +1,19 @@
-export function WebContainer() {
+
+interface WebContainerPorps {
+  url?: string | null
+}
+
+/**
+ * WebContainer is an micro-frontend solution, which is support for mutiple mime type
+ *
+ * //xxxxx.html -> iframe render
+ * //xxxxx.md | makrdown -> markdown render
+ * //xxxxx.json  -> dynamic render schema we can decide the final render continer when the json schema loaded so it's dynamic.
+ *
+ * @param porps
+ * @returns
+ */
+export function WebContainer(props: WebContainerPorps) {
   return (
     <>
     hello
